6813367980832ca38f45d126c8c08a0b1652864e
Muhammad-Fahrul <fahruldev22@gmail.com>
2024-04-17 15:39:34 +0800

    test for newStudent dynamic import from related student folder again

--- src/pages/student/studentList/StudentList.jsx
+++ src/pages/student/studentList/StudentList.jsx
@@ -12,7 +12,7 @@ import Error from '../../../components/error/Error.jsx';
 import { useGetStudentsQuery } from '../redux/studentApiSlice.js';
 import useAuth from '../../../hooks/useAuth.js';
 
-const NewStudent = lazy(() => import('./components/newStudent/NewStudent'));
+const NewStudent = lazy(() => import('./components/newStudent/NewStudent.jsx'));
 
 const StudentList = () => {
   const [screen, setScreen] = useState(false);
